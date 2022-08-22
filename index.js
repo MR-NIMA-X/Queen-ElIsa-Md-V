4d86676f58e83eb890e368bab6ec00dac4fa9891
MR-NIMA-X <104353654+MR-NIMA-X@users.noreply.github.com>
2022-08-22 16:56:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -263,7 +263,7 @@ ElisaBotMd.sendContact = async (jid, kon, quoted = '', opts = {}) => {
         //await console.log('👸💬   Automatically joining support group ...')
         console.log(`${chalk.green.bold('𝐐𝐮𝐞𝐞𝐧')}${chalk.blue.bold('𝐄𝐥𝐢𝐬𝐚')}
 ${chalk.white.bold('Version:')} ${chalk.red.bold('2.1.0')}
-${chalk.blue.italic('ℹ️ Connecting Whatsapp .')}👸💬 𝐂𝐨𝐧𝐧𝐞𝐜𝐭𝐞𝐝...`, update)
+${chalk.blue.italic('ℹ️ Connecting Whatsapp .')}👸💬 𝐂𝐨𝐧𝐧𝐞𝐜𝐭𝐞𝐝...`)
         
         
     })
