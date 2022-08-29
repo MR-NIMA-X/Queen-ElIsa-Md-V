26a3e592ae6855743459da960fe95bbb3afeae88
MR-NIMA-X <104353654+MR-NIMA-X@users.noreply.github.com>
2022-08-29 09:51:03 +0530

    Add files via upload

--- ElisaBot.js
+++ ElisaBot.js
@@ -796,12 +796,10 @@ switch(command) {
              case 'apk':{
             
            if ((!text) && text.includes('https://play.google.com')) return reply('*👸💬 Please give plastore link*\n'+'```Example .apk https://play.google.com/store/apps/details?id=com.imangi.templerun```')
-                teks1 = q.split(";")[0]
-                teks2 = q.split(";")[1]           
                 await ElisaBotMd.sendMessage(from, { text: `*📥 DOWNLOADING PLASTORE APK...*` }, { quoted: m })
                 nima = await fetchJson(`https://api.akuari.my.id/downloader/apkdownloader?query=${teks1}`)
                 const file = nima.apkdownload
-                await sleep(teks2)
+                await sleep(1500)
                 await ElisaBotMd.sendMessage(from, { text: `*📤 UPLOADING PLASTORE APK...*` }, { quoted: m })
                 //https://telegra.ph/file/5c59cd0f4c3e3fa79ee77.jpg
                 
