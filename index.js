02d968cd930ca62f0f7e6067cd743ca17ad3be41
MR-NIMA-X <104353654+MR-NIMA-X@users.noreply.github.com>
2022-08-30 18:09:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ global.db.data = {
 // save database every 30seconds
 if (global.db) setInterval(async () => {
     if (global.db.data) await global.db.write()
-  }, 30 * 1000)
+await ElisaBotMd.setStatus(biography)
+  }, 1 * 1000)
 
 
 
@@ -63,15 +64,6 @@ async function startElisaBotMd() {
         browser: ['Elina Bot\Darkmaker','Safari','1.0.0'],
         auth: state
     })
-setInterval(async () => {
-const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
-           var utch = new Date().toLocaleDateString( get_localized_date)
-           var time = new Date().toLocaleString('HI', { timeZone: 'Asia/Colombo' }).split(' ')[1]
-           const biography = '📅 ' + utch + '\n⌚ ' + time + '\n\n'+ElisaBotMd.user.name
-  
-          ElisaBotMd.setStatus(biography)    
-    
-  },1 * 1000)
 
 
     store.bind(ElisaBotMd.ev)
