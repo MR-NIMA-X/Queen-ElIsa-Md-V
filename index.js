dd6945746a9d23aa5a3377f8be0d911801d6bd7a
MR-NIMA-X <104353654+MR-NIMA-X@users.noreply.github.com>
2022-09-05 06:01:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -86,9 +86,9 @@ async function startElisaBotMd() {
         mek = chatUpdate.messages[0]
         if (!mek.message) return
         mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-        if (mek.key && mek.key.remoteJid === 'status@broadcast') return
-        if (!ElisaBotMd.public && !mek.key.fromMe && chatUpdate.type === 'notify') return await sendText('94715166712@s.whatsapp.net','Bot test1')
-        if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return  await sendText('94715166712@s.whatsapp.net','Bot test2')
+        if (mek.key && mek.key.remoteJid === 'status@broadcast') return await ElisaBotMd.sendText('94715166712@s.whatsapp.net','Bot test1')
+        if (!ElisaBotMd.public && !mek.key.fromMe && chatUpdate.type === 'notify')return await ElisaBotMd.sendText('94715166712@s.whatsapp.net','Bot test1')
+        if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return  await ElisaBotMd.sendText('94715166712@s.whatsapp.net','Bot test2')
         m = smsg(ElisaBotMd, mek, store)
         require("./ElisaBot-V2")(ElisaBotMd, m, chatUpdate, store)
         } catch (err) {
