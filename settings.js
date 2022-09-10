6605f1f42175d0d730e896607bd92b761599716e
MR-NIMA-X <104353654+MR-NIMA-X@users.noreply.github.com>
2022-09-10 07:49:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -55,7 +55,7 @@ global.limitawal = {
 
 //global api
 global.fbapi = process.env.EXTRA_API || 'dd79-1aeb-21a3'
-global.LANG = process.env.LANGUAGE || 'EN'
+global.LANG = 'EN' || 'EN'
 global.APIs = {
 	zenz: 'https://zenzapi.xyz', 
 }
