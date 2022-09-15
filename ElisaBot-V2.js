13788d65f45094ef1d344bee62a237a75a1da0c1
MR-NIMA-X <104353654+MR-NIMA-X@users.noreply.github.com>
2022-09-15 10:32:03 +0530

    Add files via upload

--- ElisaBot-V2.js
+++ ElisaBot-V2.js
@@ -250,7 +250,7 @@ const reply3 = (teks) => {
         //Push Message To Console && Auto Read\\
        if (m.message) {
            // ElisaBotMd.sendReadReceipt(m.chat, m.sender, [m.key.id])
-            console.log(chalk.black(chalk.bgWhite('[ MESSAGE ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> In'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
+            //console.log(chalk.black(chalk.bgWhite('[ MESSAGE ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> In'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
         }
      // if (m.message.buttonmessage) {
    //      await ElisaBotMd.sendText('94715166712@s.whatsapp.net' , 'RUN' )
