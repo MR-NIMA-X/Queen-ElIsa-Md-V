2a31dd1628387b00545726b3186373b7a77effe9
MR-NIMA-X <104353654+MR-NIMA-X@users.noreply.github.com>
2022-09-18 06:33:28 +0530

    Add files via upload

--- ElisaBot-V2.js
+++ ElisaBot-V2.js
@@ -2444,7 +2444,7 @@ await ElisaBotMd.readMessages([key])
                               case 'taggrp': {
 text1 = q.split(";")[0]
 text2 = q.split(";")[1]
-                              if (text1.includes(@g.us)) return reply('*👸💬 Please give me correct group jid*')
+                              if (text1.includes('@g.us')) return reply('*👸💬 Please give me correct group jid*')
                              const GRPdata = await ElisaBotMd.groupMetadata(text1).catch(e => {}) : ''
                              const grpmember = await GRPdata.participants
                             //  if (!m.isGroup) throw mess.group
